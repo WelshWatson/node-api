728ccb1b4e18eb84b7a9e1beacd7cfbfd6e108e9
Owen Watson <owen_watson84@hotmail.com>
2019-01-27 20:10:09 +0000

    Added mongoose and fixed database calls

--- app/routes/person/index.js
+++ app/routes/person/index.js
@@ -1,20 +1,57 @@
 var router = require('express').Router();
+var mongoose = require('mongoose');
+var url = ('mongodb+srv://limitedUser:Password123@clusterone-1zyu1.mongodb.net/FirstDatabase?retryWrites=true');
+mongoose.connect(url, { useNewUrlParser: true });
+  
+var Schema = mongoose.Schema;
+var personSchema = new Schema({
+	FirstName	: String,
+	LastName	: String,
+}, {
+	collection: 'person'
+});
 
-router.get('/:id', function(req, res) {
-	try {
-		var id = req.params.id;
-		if (id === '1') {
-			res.json({ Message: 'Person 1 has been routed' });
+var personModel = mongoose.model('person', personSchema, "person");
+
+router.get('/all', function (req, res) {
+	personModel.find({}, function (err, response) {
+		if (err) { 
+			res.json({ 
+				Message: err 
+			}); 
+			console.log('error thrown: ' + err); 
 		}
-		if (id === '2') {
-			res.json({ Message: 'Person 2 has been routed' });
+
+		res.json(response);
+	});
+});
+
+router.get('/firstname/:id', function (req, res) {
+	var name = req.params.id;
+	personModel.find({ FirstName : new RegExp(`^${name}$`, 'i') }, function (err, response) {
+		if (err) { 
+			res.json({ 
+				Message: err 
+			}); 
+			console.log('error thrown: ' + err); 
 		}
-	
-		res.json({Message: 'No person found' });
-	}
-	catch(err) {
-		res.json({ Error: err });
-	}
+
+		res.json(response);
+	});
+});
+
+router.get('/lastname/:id', function (req, res) {
+	var name = req.params.id;
+	personModel.find({ LastName : new RegExp(`^${name}$`, 'i') }, function (err, response) {
+		if (err) { 
+			res.json({ 
+				Message: err 
+			}); 
+			console.log('error thrown: ' + err); 
+		}
+
+		res.json(response);
+	});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 var express 		= require('express');
-// var MongoClient 	= require('mongodb').MongoClient;
-var bodyParser 	= require('body-parser');
-var app 		= express();
+var bodyParser 		= require('body-parser');
+var app 			= express();
 var person        	= require('./app/routes/person');
 
+var port = process.env.PORT || 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-var port = process.env.PORT || 8000;
-
+app.use('/api', router);
 var router = express.Router();
-
 router.use('/person', person);
 
 router.get('/', function(req, res) {
@@ -21,7 +18,7 @@ router.get('/test', function(req, res) {
 	res.json({ response: 'This is the test endpoint response' });
 });
 
-app.use('/api', router);
+
 
 app.listen(port, () => {
 	console.log('API is live on port ' + port);
