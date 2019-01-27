787a80d8c51b49f9f7bf906c855b66c4cdb1d9e3
Owen Watson <owen_watson84@hotmail.com>
2019-01-27 14:15:22 +0000

    Simple formatting changes to test new IDE

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express 		= require('express');
-const MongoClient 	= require('mongodb').MongoClient;
-const bodyParser 	= require('body-parser');
-const app 		= express();
-const person        	= require('./app/routes/person');
+var express 		= require('express');
+// var MongoClient 	= require('mongodb').MongoClient;
+var bodyParser 	= require('body-parser');
+var app 		= express();
+var person        	= require('./app/routes/person');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,11 +14,11 @@ var router = express.Router();
 router.use('/person', person);
 
 router.get('/', function(req, res) {
-	res.json({message: 'Yay it worked!'});
+	res.json({ message: 'Yay it worked!' });
 });
 
 router.get('/test', function(req, res) {
-	res.json({response: 'This is the test endpoint response' });
+	res.json({ response: 'This is the test endpoint response' });
 });
 
 app.use('/api', router);
