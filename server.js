b59ecde69e1792847a3e262f9df604c75d82ecd0
Owen <owen_watson84@hotmail.com>
2019-01-29 00:05:37 +0000

    Add index.html for root

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 var express 		= require('express');
 var bodyParser 		= require('body-parser');
-var app 			= express();
+var app 		= express();
+var path		= require('path');
 var person        	= require('./app/routes/person');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT;
+var port = process.env.PORT || 8000;
 var router = express.Router();
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 // Route all person urls here
 router.use('/person', person);
-
-// Default route
-router.get('/', function(req, res) {
-	res.json({ message: 'Default route!' });
-});
 
 // Listen to port
 app.listen(port, () => {
